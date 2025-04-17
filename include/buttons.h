5624c930f3773e092a004576c6655e0929e40ab7
Sigma-Squared <chamu.rajasekera@gmail.com>
2025-04-17 00:46:18 -0700

    working isr

--- include/buttons.h
+++ include/buttons.h
@@ -12,6 +12,8 @@ constexpr uint8_t BTN_NEXT_PIN = 25;
 constexpr uint8_t BTN_ON_OFF_PIN = 26;
 constexpr uint8_t MEDIA_ENABLED_PIN = 21;
 
+constexpr uint8_t DEBOUNCE_TIME = 250; // milliseconds
+
 enum Buttons : uint8_t
 {
     BTN_PLAY_PAUSE,
@@ -20,74 +22,86 @@ enum Buttons : uint8_t
     BTN_ON_OFF,
     BTN_COUNT
 };
+// in buttons.h (or above loop in .ino)
 
-constexpr unsigned long DEBOUNCE_TIME = 150; // milliseconds
+constexpr uint8_t BUTTON_PINS[] = {
+    BTN_PLAY_PAUSE_PIN, // 32
+    BTN_PREV_PIN,       // 33
+    BTN_NEXT_PIN,       // 25
+    BTN_ON_OFF_PIN      // 26
+};
 
-volatile bool btn_flags[BTN_COUNT]{};
-unsigned long btn_last_time[BTN_COUNT]{};
-bool media_paused = false;
+volatile bool btn_flags[BTN_COUNT] = {false};
+unsigned long btn_last_time[BTN_COUNT] = {0};
 
-void IRAM_ATTR on_play_pause() { btn_flags[BTN_PLAY_PAUSE] = true; }
-void IRAM_ATTR on_prev() { btn_flags[BTN_PREV] = true; }
-void IRAM_ATTR on_next() { btn_flags[BTN_NEXT] = true; }
-void IRAM_ATTR on_on_off() { btn_flags[BTN_ON_OFF] = true; }
+bool media_paused = true;
+
+// One ISR *per* button index:
+void IRAM_ATTR isrPlayPause() { btn_flags[BTN_PLAY_PAUSE] = true; }
+void IRAM_ATTR isrPrev() { btn_flags[BTN_PREV] = true; }
+void IRAM_ATTR isrNext() { btn_flags[BTN_NEXT] = true; }
+void IRAM_ATTR isrOnOff() { btn_flags[BTN_ON_OFF] = true; }
 
 void setup_buttons()
 {
-    pinMode(BTN_PLAY_PAUSE_PIN, INPUT_PULLUP);
-    pinMode(BTN_PREV_PIN, INPUT_PULLUP);
-    pinMode(BTN_NEXT_PIN, INPUT_PULLUP);
-    pinMode(BTN_ON_OFF_PIN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(BTN_PLAY_PAUSE_PIN), on_play_pause, FALLING);
-    attachInterrupt(digitalPinToInterrupt(BTN_PREV_PIN), on_prev, FALLING);
-    attachInterrupt(digitalPinToInterrupt(BTN_NEXT_PIN), on_next, FALLING);
-    attachInterrupt(digitalPinToInterrupt(BTN_ON_OFF_PIN), on_on_off, FALLING);
-    pinMode(MEDIA_ENABLED_PIN, OUTPUT);
-    digitalWrite(MEDIA_ENABLED_PIN, media_enabled);
+    for (int i = 0; i < BTN_COUNT; i++)
+    {
+        pinMode(BUTTON_PINS[i], INPUT_PULLUP);
+    }
+    attachInterrupt(digitalPinToInterrupt(BTN_PLAY_PAUSE_PIN),
+                    isrPlayPause, FALLING);
+    attachInterrupt(digitalPinToInterrupt(BTN_PREV_PIN),
+                    isrPrev, FALLING);
+    attachInterrupt(digitalPinToInterrupt(BTN_NEXT_PIN),
+                    isrNext, FALLING);
+    attachInterrupt(digitalPinToInterrupt(BTN_ON_OFF_PIN),
+                    isrOnOff, FALLING);
 }
 
+// then in loop(), exactly the same debounce/run logic you already have:
 void button_loop()
 {
+    unsigned long now = millis();
     for (uint8_t i = 0; i < BTN_COUNT; i++)
     {
-        if (btn_flags[i])
-        {
-            btn_flags[i] = false;
-            auto now = millis();
-            if (now - btn_last_time[i] >= DEBOUNCE_TIME)
-            {
-                btn_last_time[i] = now;
-                switch (i)
-                {
-                case BTN_PLAY_PAUSE:
-                    DEBUG_PRINTLN("Play/Pause button pressed");
-                    // if (media_paused)
-                    //     a2dp_sink->play();
-                    // else
-                    //     a2dp_sink->pause();
-                    // media_paused = !media_paused;
-                    break;
-                case BTN_PREV:
-                    DEBUG_PRINTLN("Previous button pressed");
-                    // a2dp_sink->previous();
-                    break;
-                case BTN_NEXT:
-                    DEBUG_PRINTLN("Next button pressed");
-                    // a2dp_sink->next();
-                    break;
-                case BTN_ON_OFF:
-                    DEBUG_PRINTLN("On/Off button pressed");
-                    media_enabled = !media_enabled;
-                    digitalWrite(MEDIA_ENABLED_PIN, media_enabled);
-                    if (!media_enabled)
-                    {
-                        display_large("AMP OFF");
-                    }
+        if (!btn_flags[i])
+            continue;
+        btn_flags[i] = false;
 
-                    break;
-                }
-            }
+        if (now - btn_last_time[i] < DEBOUNCE_TIME)
+            continue;
+        btn_last_time[i] = now;
+
+        if (digitalRead(BUTTON_PINS[i]) == HIGH)
+            continue;
+
+        switch (i)
+        {
+        case BTN_PLAY_PAUSE:
+            if (media_paused)
+                a2dp_sink.play();
+            else
+                a2dp_sink.pause();
+            media_paused = !media_paused;
+            DEBUG_PRINTLN("Play/Pause button pressed");
+            break;
+        case BTN_PREV:
+            a2dp_sink.previous();
+            DEBUG_PRINTLN("Previous button pressed");
+            break;
+        case BTN_NEXT:
+            a2dp_sink.next();
+            DEBUG_PRINTLN("Next button pressed");
+            break;
+        case BTN_ON_OFF:
+            media_enabled = !media_enabled;
+            digitalWrite(MEDIA_ENABLED_PIN, media_enabled);
+            if (!media_enabled)
+                display_large("AMP OFF");
+            DEBUG_PRINTLN("On/Off button pressed");
+            break;
         }
     }
 }
+
 #endif
